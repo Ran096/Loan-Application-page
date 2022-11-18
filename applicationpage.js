6926e52836afcc15057c9f51479e39756398e114
Ranj096 <ranjeetpatel096@gmail.com>
2022-11-18 23:13:27 +0530

    first commit

---
+++ applicationpage.js
@@ -0,0 +1,188 @@
+function formValidation() {
+
+    var uname = document.registration.fullname;
+    var uemail = document.registration.email;
+    if (allLetter(uname)) {
+            if (ValidateEmail(uemail)) {
+                if (ValidatePAN()) {
+                    if (valid()) {
+                    }
+                }
+
+            }
+    }
+    return false;
+
+}
+
+function allLetter(uname) {
+    var letters = /^[A-Za-z]+$/;
+    if (uname.value.match(letters)) {
+        return true;
+    }
+    else {
+        alert('Full name must have alphabet characters only');
+        uname.focus();
+        return false;
+    }
+}
+
+function ValidateEmail(uemail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (uemail.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+} function valid() {
+
+    alert('Form Succesfully Submitted');
+    window.location.reload()
+    return true;
+
+}
+
+
+function ValidatePAN() {
+     var txtPANCard = document.getElementById("txtPANCard");
+   
+    var regex = /([A-Z]){5}([0-9]){4}([A-Z]){1}$/;
+    if (regex.test(txtPANCard.value.toUpperCase())) {
+        
+        return true;
+    } else {
+       
+    alert('Invalidate Pan Card Number');
+        return false;
+    }
+}
+function onlyNumbers(evt) {
+    var e = event || evt; // For trans-browser compatibility
+    var charCode = e.which || e.keyCode;
+
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+function NumToWord(inputNumber, outputControl) {
+    var str = new String(inputNumber)
+    var splt = str.split("");
+    var rev = splt.reverse();
+    var once = ['Zero', ' One', ' Two', ' Three', ' Four', ' Five', ' Six', ' Seven', ' Eight', ' Nine'];
+    var twos = ['Ten', ' Eleven', ' Twelve', ' Thirteen', ' Fourteen', ' Fifteen', ' Sixteen', ' Seventeen', ' Eighteen', ' Nineteen'];
+    var tens = ['', 'Ten', ' Twenty', ' Thirty', ' Forty', ' Fifty', ' Sixty', ' Seventy', ' Eighty', ' Ninety'];
+
+    numLength = rev.length;
+    var word = new Array();
+    var j = 0;
+
+    for (i = 0; i < numLength; i++) {
+        switch (i) {
+
+            case 0:
+                if ((rev[i] == 0) || (rev[i + 1] == 1)) {
+                    word[j] = '';
+                }
+                else {
+                    word[j] = '' + once[rev[i]];
+                }
+                word[j] = word[j];
+                break;
+
+            case 1:
+                aboveTens();
+                break;
+
+            case 2:
+                if (rev[i] == 0) {
+                    word[j] = '';
+                }
+                else if ((rev[i - 1] == 0) || (rev[i - 2] == 0)) {
+                    word[j] = once[rev[i]] + " Hundred ";
+                }
+                else {
+                    word[j] = once[rev[i]] + " Hundred and";
+                }
+                break;
+
+            case 3:
+                if (rev[i] == 0 || rev[i + 1] == 1) {
+                    word[j] = '';
+                }
+                else {
+                    word[j] = once[rev[i]];
+                }
+                if ((rev[i + 1] != 0) || (rev[i] > 0)) {
+                    word[j] = word[j] + " Thousand";
+                }
+                break;
+
+                
+            case 4:
+                aboveTens();
+                break;
+
+            case 5:
+                if ((rev[i] == 0) || (rev[i + 1] == 1)) {
+                    word[j] = '';
+                }
+                else {
+                    word[j] = once[rev[i]];
+                }
+                if (rev[i + 1] !== '0' || rev[i] > '0') {
+                    word[j] = word[j] + " Lakh";
+                }
+                 
+                break;
+
+            case 6:
+                aboveTens();
+                break;
+
+            case 7:
+                if ((rev[i] == 0) || (rev[i + 1] == 1)) {
+                    word[j] = '';
+                }
+                else {
+                    word[j] = once[rev[i]];
+                }
+                if (rev[i + 1] !== '0' || rev[i] > '0') {
+                    word[j] = word[j] + " Crore";
+                }                
+                break;
+
+            case 8:
+                aboveTens();
+                break;
+            default: break;
+        }
+        j++;
+    }
+
+    function aboveTens() {
+        if (rev[i] == 0) { word[j] = ''; }
+        else if (rev[i] == 1) { word[j] = twos[rev[i - 1]]; }
+        else { word[j] = tens[rev[i]]; }
+    }
+
+    word.reverse();
+    var finalOutput = '';
+    for (i = 0; i < numLength; i++) {
+        finalOutput = finalOutput + word[i];
+    }
+    document.getElementById(outputControl).innerHTML = finalOutput;
+}
+
+function generateOTP() {
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 4; i++ ) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+}
+console.log( generateOTP() );
